423132a53ad236b9bd157903962787e41b73f299
Ravinder Gupta <backupnetworkten@gmail.com>
2026-07-07 13:00:26 +0530

    Update Done

--- components/video/VideosSection.tsx
+++ components/video/VideosSection.tsx
@@ -7,9 +7,9 @@ import PauseRoundedIcon from "@mui/icons-material/PauseRounded";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const videos = [
-  { video: "/videos/Video1.mp4" },
-  { video: "/videos/Video2.mp4" },
-  { video: "/videos/Video3.mp4" },
+  { video: "/videos/2.mp4" },
+  { video: "/videos/3.mp4" },
+  { video: "/videos/4.mp4" },
 ];
 
 export default function VideosSection() {
